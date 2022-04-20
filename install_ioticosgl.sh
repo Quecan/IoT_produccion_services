f0d5b3b068e81d455fc4f35a61dfdedecc83315a
jonnathan <jrqvjre@gmail.com>
2022-04-19 20:23:11 -0500

    abril19_2022

--- install_ioticosgl.sh
+++ install_ioticosgl.sh
@@ -277,7 +277,7 @@ sudo rm install_docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo git clone https://github.com/Quecan/IoT_produccion_services.git
-sudo mv sudo mv IoT_produccion_services services
+sudo mv sudo mv IoT_produccion_services  servicios
 
 
 cd services
